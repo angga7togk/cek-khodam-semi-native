c1ef31f21a0959a2268ad7da6903fe48f7be7d60
Angga7Togk <kiplihode123321@gmail.com>
2024-09-16 12:33:18 +0700

    Ajax

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-function getRandomKhodam() {
+function getRandomKhodam(khodams) {
   const randomIndex = Math.floor(Math.random() * khodams.length);
   return khodams[randomIndex];
 }
 
+const succesOutput = $(
+  '<div id="output" class="block p-2 rounded-md bg-green-500/80"></div>'
+);
+
 const cheked = {};
-$("#btn").on("click", (e) => {
+$("#btn").on("click", () => {
   const name = $("#input").val();
-  
-  if (cheked[name]) {
-    $("#output").removeClass("hidden").addClass("block");
-    $("#output").text(`Khodam kamu adalah: ${cheked[name]}`);
-  } else {
-    const khodamNew = getRandomKhodam();
-    cheked[name] = khodamNew;
-    $("#output").removeClass("hidden").addClass("block");
-    $("#output").text(`Khodam kamu adalah: ${khodamNew}`);
-  }
+
+  $.ajax({
+    type: "GET",
+    url: "https://raw.githubusercontent.com/angga7togk/cek-khodam-semi-native/main/khodam.json",
+    success: function (resData) {
+      
+      if (!cheked[name]) {
+        const jsonData = JSON.parse(resData);
+        const khodamNew = getRandomKhodam(jsonData);
+        cheked[name] = khodamNew;
+      }
+
+      succesOutput.text(`Khodam kamu adalah: ${cheked[name]}`);
+      $("#container input").before(succesOutput);
+    },
+  });
 });
-
-
